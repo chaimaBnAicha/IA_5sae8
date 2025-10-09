d4d64e4c5c73b8840960ea8f27851cdf6b0930a6
chaima154 <116753982+chaima154@users.noreply.github.com>
2025-10-09 22:50:44 +0100

    changes

---
+++ Eureka/src/test/java/com/example/eurekapi/EurekaPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
